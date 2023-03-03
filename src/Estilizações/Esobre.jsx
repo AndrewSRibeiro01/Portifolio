30b2541b31801c8898933d23e83f64c4a6f0f300
AndrewSRB <116013992+AndrewSRibeiro01@users.noreply.github.com>
2023-03-03 16:43:29 -0300

    ok

--- src/Estilizações/Esobre.jsx
+++ src/Estilizações/Esobre.jsx
@@ -36,3 +36,72 @@ export const AppContainer = styled.div`
    display:flex;
    justify-content: center
 `
+export const SubTitulo1 = styled.h3`
+    @media (max-width: 450px) {
+    font-size: 15px;
+    margin: 0 auto;
+    }
+    text-align: inherit;
+    font-size: 16px;
+    color: #FFF;
+    font-weight: 400;
+    margin-bottom: 40px;
+`
+export const Titulo1 = styled.h2`
+    @media (max-width: 450px) {
+    font-size: 20px;
+    }
+    padding: 20px;
+    margin: 0 0 5px 0;
+    color: #FFF;
+    font-size: 29px;
+    text-align: center;
+    width: 100%;
+    border-top-right-radius:11px;
+    border-top-left-radius:11px;
+`
+export const DisplayCard = styled.div`
+    justify-content: center;
+    display:flex;
+    max-width: 100%;
+`
+export const Margin = styled.div`
+    margin-top: 100px;
+`
+export const CardStyle = styled.div`
+    @media (max-width: 768px){
+    margin: 0 7px 0 7px;
+    width:420px;
+    }
+    text-align: inherit;
+    margin: 0 15px 0 0;
+    border-radius: 15px;
+    width: 680px;
+    margin: 30px;
+    background: linear-gradient(to right,#fe5d26,#370d44)
+`
+export const Imagem = styled.img`
+    @media (max-width: 768px){
+        height: 220px;
+    }
+    height: 320px;  
+    border-radius: 10px;
+    margin: 20px;
+`
+export const Card = styled.div`
+    @media (max-width: 768px){
+    flex-direction: column;
+    }
+    display:flex;
+    padding: 0 20px 20px 20px ;
+    align-items: center;
+    background: linear-gradient(to right,#fe5d26,#370d44);
+    text-align: initial;
+`
+export const CardBody = styled.div`
+    border-bottom-right-radius:11px;
+    border-bottom-left-radius:11px;
+`
+export const Div = styled.div`
+    text-align: center;
+`
--- src/components/Sobre/sobre.jsx
+++ src/components/Sobre/sobre.jsx
@@ -1,92 +1,32 @@
 import { Link } from "react-router-dom"
-import styled from "styled-components"
-import eu3 from "../../Imagens/eu4.png"
+import eu6 from "../../Imagens/eu6.png"
+import { SubTitulo1 } from "../../Estilizações/Esobre"
+import { Titulo1 } from "../../Estilizações/Esobre"
+import { DisplayCard } from "../../Estilizações/Esobre"
+import { CardStyle } from "../../Estilizações/Esobre"
+import { Imagem } from "../../Estilizações/Esobre"
+import { Card } from "../../Estilizações/Esobre"
+import { CardBody } from "../../Estilizações/Esobre"
+import { Div } from "../../Estilizações/Esobre"
+import { Margin } from "../../Estilizações/Esobre"
 
-const SubTitulo = styled.h3`
-    @media (max-width: 450px) {
-    font-size: 15px;
-    margin: 0 auto;
-    }
-    text-align: inherit;
-    font-size: 16px;
-    color: #FFF;
-    font-weight: 400;
-    margin-bottom: 40px;
-`
-const Titulo = styled.h2`
-    @media (max-width: 450px) {
-    font-size: 20px;
-    }
-    padding: 20px;
-    margin: 0 0 5px 0;
-    color: #FFF;
-    font-size: 29px;
-    text-align: center;
-    width: 100%;
-    border-top-right-radius:11px;
-    border-top-left-radius:11px;
-`
-const DisplayCard = styled.div`
-    justify-content: center;
-    display:flex;
-    max-width: 100%;
-`
-const Margin = styled.div`
-    margin-top: 100px;
-`
-const CardStyle = styled.div`
-    @media (max-width: 768px){
-    margin: 0 7px 0 7px;
-    width:420px;
-    }
-    text-align: inherit;
-    margin: 0 15px 0 0;
-    border-radius: 15px;
-    width: 680px;
-    margin: 30px;
-    background: linear-gradient(to right,#fe5d26,#370d44)
-`
-const Imagem = styled.img`
-    @media (max-width: 768px){
-        height: 220px;
-    }
-    height: 320px;  
-    border-radius: 10px;
-    margin: 20px;
-`
-const Card = styled.div`
-    @media (max-width: 768px){
-    flex-direction: column;
-    }
-    display:flex;
-    padding: 20px;
-    align-items: center;
-    background: linear-gradient(to right,#fe5d26,#370d44);
-    text-align: initial;
-`
-const CardBody = styled.div`
-    border-bottom-right-radius:11px;
-    border-bottom-left-radius:11px;
-`
-const Div = styled.div`
-    text-align: center;
-`
+
 const Sobre = () => {
     return (
         <Margin>
             <DisplayCard>
                 <CardStyle class="card">
-                    <Titulo>Um pouco sobre mim! <hr /> </Titulo>
+                    <Titulo1>Um pouco sobre mim! <hr /> </Titulo1>
                     <Div>
                         <Card>
-                            <Imagem src={eu3} class="card-img-top" alt="Eu" />
-                            <SubTitulo>
+                            <Imagem src={eu6} class="card-img-top" alt="Eu" />
+                            <SubTitulo1>
                                 Me chamo Andrew, tenho 24 anos, trabalho como Quality Assurance na empresa ArsenalCar| Digigrow.
                                 <br /> <br />
                                 Moro em Santa Isabel SP, faço faculdade de Gestão da tecnologia da informação
                                 <br /> <br />
                                 Estou sempre buscando por novos conhecimentos!
-                            </SubTitulo>
+                            </SubTitulo1>
                         </Card>
                         <button style={{ fontWeight: "500", width: "200px", borderRadius: "15px", textDecoration: "none" }} type="button" class="btn btn-light btn-lg"> < Link to={"/contato"} style={{ textDecoration: "none", color: "black" }}> Contato</Link></button>
                     </Div>
