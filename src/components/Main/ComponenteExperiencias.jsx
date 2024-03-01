b92d153f0fea50b758c11ee610c778cfd7a4b876
Andrew Souza <andrew.souza@aigroup.local>
2024-03-01 16:37:17 -0300

    fix: correctly image

--- src/components/Main/ComponenteExperiencias.jsx
+++ src/components/Main/ComponenteExperiencias.jsx
@@ -7,7 +7,8 @@ const ComponenteExperiencias = ({ data, cargo, texto }) => {
         <Div>
             <CardExperiencias>
                 <Logoempresa>
-                    <img src="https://app.digigrow.com.br/97b986d5f83334bdf2f4adf2bb24394d.svg" />
+                    <img style={{ width: "15%", height: "15%" }}
+                        src="https://s3-alpha-sig.figma.com/img/d2c2/6e19/7a78fe314dc422c3452be49e90ae6082?Expires=1710115200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DOh5Y5B9F2VKtmNlztU~ccEUfbptGU4wNrOHQFO3tZ1X92O5nIpWWFQLYMCO0KefLiRYhY26J6j3di1bWpsufH3F7j-CvzJDpKksE3NXkaR0AgEavh98KJ0wh7EkjEoibScLShUwLGE~Pb1zpp-UaBw9hiayZt9Lu0WJ8n0RzUQqzTxZltPcDeQ~OFfsdhAyyD19D~cBCMBK5SEgkwU7pSlRBDwwdjv6MHD69Bv0j4UBMdydfrVodtDExvzNZ6K1EZFwsKO3mu5aHa0016VQVkbyyxXMUlQkkAtmA9mIT074mxmGVqPRS9LYg0q07C-PTCG1j6G9si-21GLWr1dXlQ__" />
                 </Logoempresa>
                 <H1Experiencia>
                     {cargo}
