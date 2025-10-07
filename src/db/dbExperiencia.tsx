080da2ef12e3065ba036be4405069e90295de6ff
Andrew de Souza ribeiro <andrewsouza57@gmail.com>
2025-10-07 14:54:57 -0300

    ok

--- src/db/dbExperiencia.tsx
+++ src/db/dbExperiencia.tsx
@@ -7,13 +7,13 @@ const DbExperiencia = [{
         texto: "Com experiência em front-end e back-end, criando aplicações modernas, performáticas e escaláveis. Trabalho com tecnologias como JavaScript, TypeScript, Angular, Node.js, Nest.js e banco de dados não-relacionais. Tenho foco em escrever código limpo, seguir boas práticas e entregar soluções eficientes.",
     },
     {
-    data: "Jan 2024 - Abril 2025",
+    data: "Jan 2023 - Maio 2025",
     imagem: digigrow,
     cargo: "Desenvolvedor de Software",
     texto: "Como desenvolvedor Full Stack,construí soluções digitais de ponta a ponta, unindo expertise no front-end para criar interfaces intuitivas e atraentes, com habilidades no back-end para implementar lógica robusta e gerenciar dados eficientemente.",
 },
 {
-    data: "Nov 2022 - Dez 2023",
+    data: "Nov 2022 - Jan 2023",
     imagem: digigrow,
     cargo: "QA, Quality Assurance",
     texto: "Contribuí ativamente para a qualidade de projetos desafiadores na Digigrow, destacando-me na resolução eficaz de problemas. Minha experiência abrange testes abrangentes, automação eficiente e colaboração estreita com equipes multifuncionais. Priorizando a excelência em qualidade, alcancei metas consistentes de entrega de software confiável na Digigrow.",
